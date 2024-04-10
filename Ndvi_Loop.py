df6f8a8a540ac4929856e4039e534f1a6307a8a9
soarwing52 <hung.che.yi@gmail.com>
2024-04-10 20:58:03 +0800

    fix ndvi

--- Ndvi_Loop.py
+++ Ndvi_Loop.py
@@ -3,15 +3,17 @@ import os
 import arcpy
 from arcpy.sa import *
 arcpy.env.Overwrite = True
+arcpy.env.scratchWorkspace = os.path.dirname(r"D:\temp")
 
-RESULT_FOLDER = 'D:\\NDVI_R'
-TARGET_FOLDER = r'D:\ndvi\Bulk Order ndvi\Landsat 8 OLI_TIRS C2 L1'
+RESULT_FOLDER = r'D:\NDVI_R'
+TARGET_FOLDER = r'D:\NDVI'
 
-def ndvi(file):
+def ndvi(dir):
     # file = raw_input(
     #     "your folder of image(delete the .tar if had\n ex.D:\GIS\LC08_L1TP_118044_20161024_20170318_01_T1): \n")
-    path_red = file + "\\" + file[-40:] + "_B4.tif"
-    path_nir = file + "\\" + file[-40:] + "_B5.tif"
+
+    path_red = dir + "\\" + dir[-40:] + "_B4.tif"
+    path_nir = dir + "\\" + dir[-40:] + "_B5.tif"
 
     red_band_raster = Raster(path_red)
     nir_band_raster = Raster(path_nir)
@@ -22,13 +24,41 @@ def ndvi(file):
     Rfloat = Float(red_band_raster)
     Nfloat = Float(nir_band_raster)
     ndvi = Divide((Nfloat - Rfloat), (Nfloat + Rfloat))
-    location = file[-30:-24]
-    date = file[-24:-15]
+    location = dir[-30:-24]
+    date = dir[-24:-15]
     name = date + "_" + location
     print name
     path = RESULT_FOLDER + r"\\ndvi_" + name + ".tif"
     print path
     ndvi.save(path)
+
+def new_ndvi(dir):
+    files = os.listdir(dir)
+    # print files
+    b4,b5="",""
+    for file in files:
+        if "_B4" in file:
+            b4 = file
+        elif "_B5" in file:
+            b5 = file
+
+    path_red = dir + "\\" + b4
+    path_nir = dir + "\\" + b5
+
+    print b4,b5
+
+    red_band_raster = Raster(path_red)
+    nir_band_raster = Raster(path_nir)
+    print red_band_raster
+    print nir_band_raster
+    print "band condirmed"
+    
+    Rfloat = Float(red_band_raster)
+    Rfloat.save("Rfloat.tif")
+    Nfloat = Float(nir_band_raster)
+    Nfloat.save("Nfloat.tif")
+    ndvi = Divide((Nfloat - Rfloat), (Nfloat + Rfloat))
+    ndvi.save("ndvi.tif")
 
 if __name__ == '__main__':
     print("GO")
@@ -36,4 +66,4 @@ if __name__ == '__main__':
         file_path = os.path.join(TARGET_FOLDER, x)
         if os.path.isdir(file_path):
             print(file_path)
-            ndvi(file_path)
+            new_ndvi(file_path)
